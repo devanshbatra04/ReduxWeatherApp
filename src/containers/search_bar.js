6c83c7c8007026003efa84edea4f58401cca07b9
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-12 15:57:11 +0530

    Search Bar container initialised

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+    }
+    onInputChange(event){
+        console.log(event.target.value);
+    }
+
     render(){
         return (
             <form className="input-group">
-                <input type="text"/>
+                <input
+                    placeholder="get a five day forecast in your favourite city"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                    type="text"/>
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">
                         Submit
