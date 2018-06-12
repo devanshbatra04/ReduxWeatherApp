4ceefbd926d17d252281c03305ac4f5921d6990a
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-12 16:08:02 +0530

    search bar state working :D

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -8,9 +8,11 @@ export default class SearchBar extends Component {
         this.state = {
             term: ''
         };
+
+        this.onInputChange = this.onInputChange.bind(this);
     }
     onInputChange(event){
-        console.log(event.target.value);
+        this.setState({term: event.target.value})
     }
 
     render(){
