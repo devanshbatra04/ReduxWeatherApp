c28c5fe0f4fbd62ffaac0fcc7db9c759a47501a5
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-14 01:13:58 +0530

    typo corrected

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,6 +3,13 @@ import {connect} from 'react-redux';
 
 
 class WeatherList extends Component {
+    renderWeather(cityData){
+        return (
+            <tr>
+                <td>{cityData.city.name}</td>
+            </tr>
+        )
+    }
     render(){
         return (
             <table className="table table-hover">
@@ -15,7 +22,7 @@ class WeatherList extends Component {
                     </tr>
                 </thead>
                 <tbody>
-
+                {this.props.weather.map(this.renderWeather)}
                 </tbody>
 
             </table>
