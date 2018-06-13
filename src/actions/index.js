2e0cd441c5a2cad43180e36871422551637adbe1
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-13 22:16:59 +0530

    axios.get functionality added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_Key = require('../../apiKeys').OWM;
 const rootURL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_Key}`;
 
@@ -5,7 +7,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${rootURL}&q=${city},us`;
+    const request = axios.get(url);
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
