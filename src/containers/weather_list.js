5c64b6b1b7ae8aacf9b61ef8672bf244d1bd1c92
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-14 04:57:52 +0530

    App completed

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import Chart from "../components/charts";
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import GoogleMap from '../components/google_maps';
 
 
 class WeatherList extends Component {
     renderWeather(cityData){
         console.log(cityData);
         const name = cityData.city.name;
-
+        const {lon,lat} = cityData.city.coord;
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td>
                     <Chart data={temps} color="orange" units="K"/>
                 </td>
