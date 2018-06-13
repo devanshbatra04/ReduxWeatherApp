f97e234b0b28ee4004971f85f65c4e46aa23b754
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-14 02:58:26 +0530

    pressure and humidity added

--- src/components/charts.js
+++ src/components/charts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Sparklines, SparklinesLine} from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
 
 export default (props) => {
     console.log(props.data, props.color);
@@ -7,6 +7,7 @@ export default (props) => {
         <div>
             <Sparklines height={120} width={180} data={props.data}>
                 <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
             </Sparklines>
         </div>
     )
