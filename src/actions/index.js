5489c4eb0f61409bca64e5fc333bece59f5ac54e
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-13 22:11:04 +0530

    API key added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 const API_Key = require('../../apiKeys').OWM;
+const rootURL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_Key}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
